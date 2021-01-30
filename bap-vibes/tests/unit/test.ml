4317e6094ee10b69782e995f6f34e4e056593532
JT Paasch <jt.paasch@gmail.com>
2021-01-30 18:55:38 -0500

    Separated integration and unit tests

--- bap-vibes/tests/unit/test.ml
+++ bap-vibes/tests/unit/test.ml
@@ -4,11 +4,11 @@ let suite = "Full suite" >::: [
     "Exe_ingester" >::: Test_exe_ingester.suite;
     "Patch_ingester" >::: Test_patch_ingester.suite;
     "Compiler" >::: Test_compiler.suite;
-    "Vibes_ir" >::: Test_ir.suite;
+    "Ir" >::: Test_ir.suite;
     "Minizinc" >::: Test_minizinc.suite;
     "Patcher" >::: Test_patcher.suite;
     "Verifier" >::: Test_verifier.suite;
-    "Arm_gen" >::: Test_arm.suite;
+    "Arm_selector" >::: Test_arm_selector.suite;
   ]
 
 let _ = run_test_tt_main suite
