ff0105fb29b61b62fdc37f57f9cfa2322ad8e390
Chris Casinghino <chris.casinghino@gmail.com>
2021-02-25 16:42:14 -0500

    Add thumb examples in resources/exe, also rename existing examples
    This:
    
    1) Adds thumb versions of simple-compiled and simple-multi
    2) Renames the existing examples to distinguish the versions (they now all
       start with "arm-" or "thumb-"
    3) Fixes the reference binaries / config file in the exisitng simple-compiled
       example for arm.  The old ones used the hf compiler.

--- plugin/tests/test_arm_exes.ml
+++ plugin/tests/test_arm_exes.ml
@@ -4,11 +4,11 @@ module H = Helpers
 
 (* Check patching the "simple" exe example. *)
 let test_simple (ctxt : test_ctxt) : unit =
-  H.run_make ["clean"] ~dir:"simple" ~ctxt;
-  H.run_make ["patch"] ~dir:"simple" ~ctxt;
-  H.run_arm_exe ["main"] ~dir:"simple" ~exit_code:5 ~ctxt;
-  H.run_arm_exe ["main.patched"] ~dir:"simple" ~exit_code:3 ~ctxt;
-  H.run_make ["clean"] ~dir:"simple" ~ctxt
+  H.run_make ["clean"] ~dir:"arm-simple" ~ctxt;
+  H.run_make ["patch"] ~dir:"arm-simple" ~ctxt;
+  H.run_arm_exe ["main"] ~dir:"arm-simple" ~exit_code:5 ~ctxt;
+  H.run_arm_exe ["main.patched"] ~dir:"arm-simple" ~exit_code:3 ~ctxt;
+  H.run_make ["clean"] ~dir:"arm-simple" ~ctxt
 
 (* Check patching the "simple-cegis" exe example. *)
 let test_simple_cegis (ctxt : test_ctxt) : unit =
@@ -20,19 +20,19 @@ let test_simple_cegis (ctxt : test_ctxt) : unit =
 
 (* Check patching the "simple-compiled" exe example. *)
 let test_simple_compiled (ctxt : test_ctxt) : unit =
-  H.run_make ["clean"] ~dir:"simple-compiled" ~ctxt;
-  H.run_make ["patch"] ~dir:"simple-compiled" ~ctxt;
-  H.run_arm_exe ["main"] ~dir:"simple-compiled" ~exit_code:5 ~ctxt;
-  H.run_arm_exe ["main.patched"] ~dir:"simple-compiled" ~exit_code:3 ~ctxt;
-  H.run_make ["clean"] ~dir:"simple-compiled" ~ctxt
+  H.run_make ["clean"] ~dir:"arm-simple-compiled" ~ctxt;
+  H.run_make ["patch"] ~dir:"arm-simple-compiled" ~ctxt;
+  H.run_arm_exe ["main"] ~dir:"arm-simple-compiled" ~exit_code:5 ~ctxt;
+  H.run_arm_exe ["main.patched"] ~dir:"arm-simple-compiled" ~exit_code:3 ~ctxt;
+  H.run_make ["clean"] ~dir:"arm-simple-compiled" ~ctxt
 
 (* Check patching the "simple-multi" exe example. *)
 let test_simple_multi (ctxt : test_ctxt) : unit =
-  H.run_make ["clean"] ~dir:"simple-multi" ~ctxt;
-  H.run_make ["patch"] ~dir:"simple-multi" ~ctxt;
-  H.run_arm_exe ["main"] ~dir:"simple-multi" ~exit_code:0 ~ctxt;
-  H.run_arm_exe ["main.patched"] ~dir:"simple-multi" ~exit_code:1 ~ctxt;
-  H.run_make ["clean"] ~dir:"simple-multi" ~ctxt
+  H.run_make ["clean"] ~dir:"arm-simple-multi" ~ctxt;
+  H.run_make ["patch"] ~dir:"arm-simple-multi" ~ctxt;
+  H.run_arm_exe ["main"] ~dir:"arm-simple-multi" ~exit_code:0 ~ctxt;
+  H.run_arm_exe ["main.patched"] ~dir:"arm-simple-multi" ~exit_code:1 ~ctxt;
+  H.run_make ["clean"] ~dir:"arm-simple-multi" ~ctxt
 
 let suite = [
   "Test: simple" >:: test_simple;
