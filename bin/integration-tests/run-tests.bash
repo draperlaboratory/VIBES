b91c8b1ab3d12d6a38c29d68169058913dfcfbb9
JT Paasch <jt.paasch@gmail.com>
2021-04-09 16:15:53 -0400

    More massaging of bin scripts

--- bin/integration-tests/run-tests.bash
+++ bin/integration-tests/run-tests.bash
@@ -6,8 +6,8 @@
 
 # Define some paths.
 THIS_SCRIPT="${BASH_SOURCE[0]}"
-SETUP_DIR="$(cd "$(dirname "${THIS_SCRIPT}")" && pwd)"
-COMMON_LIB_DIR="$(cd "${SETUP_DIR}/../common-lib" && pwd)"
+THIS_DIR="$(cd "$(dirname "${THIS_SCRIPT}")" && pwd)"
+COMMON_LIB_DIR="$(cd "${THIS_DIR}/../common-lib" && pwd)"
 
 # Include the relevant libraries.
 . "${COMMON_LIB_DIR}/utils.bash"
