2fc261befe58193984b723446a0c394ebcb974d2
Cody Roux <croux@draper.com>
2021-05-18 12:33:38 -0400

    Rewrite the instruction selection to generate fresh variables for each node that matches a pattern leaf, preparing for maximal munch.

--- bap-vibes/src/gir.ml
+++ bap-vibes/src/gir.ml
@@ -31,37 +31,81 @@ module Pattern_node = struct
 
   let slot = Knowledge.Class.property Theory.Program.cls "pattern-node" domain
 
-  type 't match_res = Assign of var * 't | Trivial | Fail
+  (* Placeholder for Ir.t *)
+  type ir = {
+    tag : tid;
+    mk_ir :
+      inputs:(var Var.Map.t) ->
+      (* Just one variable for now *)
+      outputs:var ->
+      (* Just a single operation for now *)
+      Ir.operation
+  }
 
-  let match_ (pat : t) (n : Ast_node.t) (tm : 't) : 't match_res =
-      match pat with
-      | Meta v -> Assign (v, tm)
-      | Concrete m ->
-        if Ast_node.equal m n then Trivial else Fail
+  let equal_ir a b = Tid.(a.tag = b.tag)
 
-  type 't subst = 't Var.Map.t option
+  let emit_domain = Knowledge.Domain.optional ~equal:equal_ir "ir-domain"
 
-  let init_subst : 't subst = Some Var.Map.empty
+  let emit_slot = Knowledge.Class.property Theory.Program.cls "emit-ir" emit_domain
 
-  let add_option k v m =
-    match Var.Map.add m ~key:k ~data:v with
-    | `Duplicate -> None
-    | `Ok m -> Some m
+  type 't match_res =
+      Assign of { pat_var : var; tmp_var : var; binding : 't }
+    | Trivial
+    | Fail
 
-  let add_res (res : 't match_res) (s : 't subst) : 't subst =
-    match res, s with
-    | Assign (v, t), Some m -> add_option v t m
-    | Trivial, om -> om
+  let match_ (pat : t) (n : Ast_node.t) (binding : 't) : 't match_res =
+    match pat with
+    | Meta v ->
+      let temp = Var.create ~fresh:true "tmp" Unk in
+      Assign { pat_var = v; tmp_var = temp; binding = binding }
+    | Concrete m ->
+      if Ast_node.equal m n then Trivial else Fail
+
+  (* The result of a substitution is a map, which assigns pattern
+     variables to a pair of a temporary and the term at that leaf, and
+     an "emit" list which is a list of instructions to emit. *)
+  type ('v, 'a) subst_res = { map : (var * 'v) Var.Map.t; emit : 'a list }
+
+  type ('v, 'a) subst = ('v, 'a) subst_res option
+
+  let init_subst : ('v, 'a) subst = Some { map = Var.Map.empty; emit = [] }
+
+  let add_binding k v m =
+    match m with
+    | Some { map; emit } ->
+      begin
+        match Var.Map.add map ~key:k ~data:v with
+        | `Duplicate -> None
+        | `Ok m -> Some { map = m; emit = emit }
+      end
+    | None -> None
+
+  let emit e m =
+    { m with emit = e::m.emit }
+
+  let add_emit (e : 'a option) (s : ('v, 'a) subst) : ('v, 'a) subst =
+    match e, s with
+    | Some e, Some s -> Some { s with emit = e::s.emit }
     | _ -> None
+
+  let add_res (res : 'v match_res) (e : 'a option) (s : ('v, 'a) subst) : ('v, 'a) subst =
+    match res with
+    | Assign { pat_var; tmp_var; binding } -> add_binding pat_var (tmp_var, binding) s
+    (* Note: we only emit on a non-variable node *)
+    | Trivial -> add_emit e s
+    | Fail -> None
 
   (* Left biaised merge because this doesn't exist in Core_kernel
          apparently *)
-  let merge m1 m2 =
-    Var.Map.fold m1 ~init:m2
-      ~f:(fun ~key ~data om -> Option.bind om ~f:(add_option key data))
+  let merge (m1 : ('v, 'a) subst_res) (m2 : ('v, 'a) subst) =
+    let res = Var.Map.fold m1.map ~init:m2
+        ~f:(fun ~key ~data om -> add_binding key data om)
+    in
+    List.fold m1.emit ~init:res
+      ~f:(fun om e -> add_emit (Some e) om)
 
   (* Again: we fail here if there are non-linear patterns *)
-  let join_subst (s1 : 't subst) (s2 : 't subst) : 't subst =
+  let join_subst (s1 : ('v, 'a) subst) (s2 : ('v, 'a) subst) : ('v, 'a) subst =
     match s1, s2 with
     | Some m1, m2 -> merge m1 m2
     | _ -> None
@@ -77,10 +121,11 @@ type 'lab ast = 'lab ast_raw KB.t
 
 
 (* Creates a fresh AST node with prescribed label and children *)
-let mk_app
+let mk_tagged_app
     (slot : (Theory.program, 'lab option) Knowledge.slot)
     (lab : 'lab)
     (args : 'lab ast seq)
+    (tag : Pattern_node.ir option)
   : 'lab ast =
   let* node_tid = KB.Object.create Theory.Program.cls in
   let node = AST.Node.create node_tid in
@@ -104,11 +149,20 @@ let mk_app
       ~init:graph
       ~f:(fun g e -> AST.Edge.insert e g)
   in
-  let+ () = KB.provide slot node (Some lab) in
+  let* () = KB.provide slot node (Some lab) in
+  let+ () =
+    if Option.is_some tag then
+      KB.provide Pattern_node.emit_slot node tag
+    else
+      KB.return ()
+  in
   { node; graph }
 
+let mk_app slot lab args = mk_tagged_app slot lab args None
 
 let mk_const slot lab = mk_app slot lab Seq.empty
+
+let mk_meta var = mk_const Pattern_node.slot (Pattern_node.Meta var)
 
 let get_pat_hd p =
   let+ p = KB.collect Pattern_node.slot (AST.Node.label p.node) in
@@ -128,19 +182,30 @@ let get_children t =
         let tid = AST.Edge.dst e in
         { node = tid; graph = t.graph })
 
+let fresh_var = Var.create ~fresh:true "dummy" Unk
+
 let rec match_ (pat : Pattern_node.t ast_raw) (tm : Ast_node.t ast_raw)
-  : (Ast_node.t ast_raw) Pattern_node.subst KB.t =
+  : (Ast_node.t ast_raw, Ir.operation) Pattern_node.subst KB.t =
   let* pat_hd = get_pat_hd pat in
   let* tm_hd = get_tm_hd tm in
   let pat_children = get_children pat in
   let tm_children = get_children tm in
-  let+ subst = match_seq pat_children tm_children in
+  let* subst = match_seq pat_children tm_children in
+  let+ action = KB.collect Pattern_node.emit_slot pat.node in
+  let action = Option.map2 subst action
+      ~f:(fun sub act ->
+          let inputs = Var.Map.map sub.map ~f:fst in
+          act.Pattern_node.mk_ir
+            ~inputs:inputs
+            ~outputs:fresh_var)
+  in
   Pattern_node.add_res
     (Pattern_node.match_ pat_hd tm_hd tm)
+    action
     subst
 
 and match_seq (pats : Pattern_node.t ast_raw list) (tms : Ast_node.t ast_raw list)
-  : (Ast_node.t ast_raw) Pattern_node.subst KB.t =
+  : (Ast_node.t ast_raw, Ir.operation) Pattern_node.subst KB.t =
   let pairs = List.zip_exn pats tms in
   KB.List.fold pairs
     ~init:Pattern_node.init_subst
@@ -171,15 +236,25 @@ let test_tm =
   let arg_2 = mk_const const_2 in
   mk_app add (Seq.of_list [arg_1; arg_2])
 
+
+(* This could be done with some hidden utilities from Arm_selector *)
+let arm_add ~inputs ~outputs =
+  let x = Var.Map.find_exn inputs (Var.create "X" (Imm 32)) |> Ir.simple_var in
+  let y = Var.Map.find_exn inputs (Var.create "Y" (Imm 32)) |> Ir.simple_var in
+  let z = outputs |> Ir.simple_var in
+  let add_op = Ir.Opcode.create ~arch:"arm" "add" in
+  Ir.simple_op add_op (Ir.Var z) [(Ir.Var x); (Ir.Var y)]
+
+
 let test_pat =
-  let mk_app = mk_app Pattern_node.slot in
-  let mk_const = mk_const Pattern_node.slot in
-  let var_1 = Pattern_node.Meta (Var.create "X" (Imm 32)) in
-  let var_2 = Pattern_node.Meta (Var.create "Y" (Imm 32)) in
+  let mk_app = mk_tagged_app Pattern_node.slot in
+  let var_1 = Var.create "X" (Imm 32) in
+  let var_2 = Var.create "Y" (Imm 32) in
   let add = Pattern_node.Concrete (Ast_node.Operation "add") in
-  let arg_1 = mk_const var_1 in
-  let arg_2 = mk_const var_2 in
+  let arg_1 = mk_meta var_1 in
+  let arg_2 = mk_meta var_2 in
   mk_app add (Seq.of_list [arg_1; arg_2])
+    (Some { tag = Tid.create (); mk_ir = arm_add })
 
 let lift_match p t = KB.Lift.binary match_ p t |> KB.join
 
@@ -193,8 +268,12 @@ let () =
           Printf.printf "\n\nMatch failed!\n\n"
         else
           begin
-          Printf.printf "\n\nMatch success!\n\n";
-          Var.Map.iter_keys (Option.value_exn subst)
-            ~f:(fun v -> Format.printf "%a\n" Var.pp v)
-        end
+            let Pattern_node.{ map; emit } = Option.value_exn subst in
+            Printf.printf "\n\nMatch success!\n\n";
+            Var.Map.iteri map
+              ~f:(fun ~key:v ~data:tmp ->
+                  Format.printf "%a |-> %a %a\n" Var.pp v Var.pp (fst tmp) Tid.pp (snd tmp).node);
+            Core_kernel.List.iter emit
+              ~f:(function o -> Format.printf "%s\n" (Ir.operation_to_string o))
+          end
       end)
