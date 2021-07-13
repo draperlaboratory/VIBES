793d980ac5a6d99f05a921ed302720ae7b6ad339
Cody Roux <croux@draper.com>
2021-07-13 16:41:25 -0400

    Update the tests so that they typecheck. Still fail for now!

--- bap-vibes/tests/unit/test_parse_c.ml
+++ bap-vibes/tests/unit/test_parse_c.ml
@@ -1,26 +1,49 @@
 open Bap_vibes
 open OUnit2
 open Core_kernel
+open Bap_core_theory
+open Bap_knowledge
+open Knowledge.Syntax
+open Knowledge.Let
 
-let assert_parse_eq s1 s2 = 
-    match Parse_c.c_patch_to_sexp_string s1 with
-    | Error e -> assert_failure (Printf.sprintf "FrontC failed to parse %s\n with error %s" s1 e)
-    | Ok s1 ->
-        assert_equal ~cmp:Sexp.equal
-        ~printer:Sexp.to_string
-        (Sexp.of_string s2)
-        (Sexp.of_string ("("^s1^")"))
+let assert_parse_eq s1 s2 =
+  match Parse_c.parse_c_patch s1 with
+    | Error e ->
+      assert_failure
+        (Printf.sprintf "FrontC failed to parse %s\n with error %s" s1 e)
+    | Ok ast ->
+      Bap.Std.Toplevel.exec
+        begin
+          Theory.instance () >>=
+          Theory.require >>= fun (module T) ->
+          let module Eval = Parse_c.Eval(T) in
+          let* sem = Eval.c_patch_to_eff Helpers.dummy_target ast in
+          let sem_str = Bap.Std.Insn.to_string sem in
+          KB.return @@
+          assert_equal ~cmp:String.equal ~printer:ident sem_str s2
+        end
 
 
 let test_var_decl _ = assert_parse_eq "int x, y, z;" "((var-decls x y z))"
-let test_assign _ = assert_parse_eq "x = y;" "((var-decls)(set x y))"
-let test_ite _ = assert_parse_eq "if(cond_expr){goto l1;}else{goto l2;};"
-        "((var-decls) (branch cond_expr (goto l1) (goto l2)))"
-let test_fallthrough _ = assert_parse_eq "goto fallthrough;"
-  "((var-decls) fallthrough)"
-let test_array _ = assert_parse_eq "y = a[7];"
-  "((var-decls) (set y (load (+ a 7))))"
-let test_compound _ = assert_parse_eq 
+
+let test_assign _ = assert_parse_eq "int x, y; x = y;" "((var-decls)(set x y))"
+
+let test_ite _ =
+  assert_parse_eq
+    "int cond_expr; if(cond_expr){goto l1;}else{goto l2;};"
+    "((var-decls) (branch cond_expr (goto l1) (goto l2)))"
+
+let test_fallthrough _ =
+  assert_parse_eq
+    "goto fallthrough;"
+    "((var-decls) fallthrough)"
+
+let test_array _ =
+  assert_parse_eq
+    "int a, y; y = a[7];"
+    "((var-decls) (set y (load (+ a 7))))"
+
+let test_compound _ = assert_parse_eq
   "int x, y, z;
   char q;
   x = 0x7;
@@ -31,7 +54,7 @@ let test_compound _ = assert_parse_eq
       goto larry;
   }"
   "
-  ((var-decls x y z q) 
+  ((var-decls x y z q)
   (set x 0x7)
   (set x (load y))
   (branch (> x 0) (goto fred) (goto larry))
