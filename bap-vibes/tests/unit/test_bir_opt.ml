00a2f6fc158e54343f0849d24ef6450c9f60304a
Cody Roux <croux@draper.com>
2021-08-05 17:35:29 -0400

    Minor cleanup for the optimizer + tests.

--- bap-vibes/tests/unit/test_bir_opt.ml
+++ bap-vibes/tests/unit/test_bir_opt.ml
@@ -8,33 +8,33 @@ let indirect_tgt = Bil.int Bitvector.(of_int ~width:32 42)
 
 let cond = Var.create "cond" (Bil.Imm 32)
 
-let blk1 =
+let blk_redir =
   let kind = Goto (Indirect indirect_tgt) in
   let jmp = Jmp.create kind in
   Blk.create ~jmps:[jmp] ()
 
-let blk1_1 =
+let blk_cond_redir =
   let kind = Goto (Indirect indirect_tgt) in
   let jmp = Jmp.create ~cond:(Bil.Var cond) kind in
   Blk.create ~jmps:[jmp] ()
 
-let blk1_2 =
+let blk_mov_redir =
   let kind = Goto (Indirect indirect_tgt) in
   let mov = Def.create cond (Bil.var cond) in
-  let jmp = Jmp.create ~cond:(Bil.Var cond) kind in
+  let jmp = Jmp.create kind in
   Blk.create ~defs:[mov] ~jmps:[jmp] ()
 
-let blk2 =
-  let kind = Goto (Direct (Term.tid blk1)) in
+let blk_dir =
+  let kind = Goto (Direct (Term.tid blk_redir)) in
   let jmp = Jmp.create ~cond:(Bil.Var cond) kind in
   Blk.create ~jmps:[jmp] ()
 
 (* Tests that the optimization actually happened *)
 let test_success _ =
-  let blks = [blk1; blk2] in
+  let blks = [blk_redir; blk_dir] in
   let opts = Bir_opt.apply blks in
-  let blk2 = List.find opts ~f:(fun b -> Tid.(Term.tid b = Term.tid blk2)) in
-  match Option.map blk2 ~f:(fun b -> Term.enum jmp_t b |> Seq.to_list) with
+  let blk_dir = List.find opts ~f:(fun b -> Tid.(Term.tid b = Term.tid blk_dir)) in
+  match Option.map blk_dir ~f:(fun b -> Term.enum jmp_t b |> Seq.to_list) with
   | None -> assert_failure "didn't find block!"
   | Some [j] ->
     begin
@@ -50,10 +50,10 @@ let test_success _ =
 
 (* Optimization shouldn't happen because the second jump is conditional *)
 let test_failure_branch _ =
-  let blks = [blk1_1; blk2] in
+  let blks = [blk_cond_redir; blk_dir] in
   let opts = Bir_opt.apply blks in
-  let blk2 = List.find opts ~f:(fun b -> Tid.(Term.tid b = Term.tid blk2)) in
-  match Option.map blk2 ~f:(fun b -> Term.enum jmp_t b |> Seq.to_list) with
+  let blk_dir = List.find opts ~f:(fun b -> Tid.(Term.tid b = Term.tid blk_dir)) in
+  match Option.map blk_dir ~f:(fun b -> Term.enum jmp_t b |> Seq.to_list) with
   | None -> assert_failure "didn't find block!"
   | Some [j] ->
     begin
@@ -66,10 +66,10 @@ let test_failure_branch _ =
 
 (* Optimization shouldn't happen because the second block has data effects *)
 let test_failure_mov _ =
-  let blks = [blk1_2; blk2] in
+  let blks = [blk_mov_redir; blk_dir] in
   let opts = Bir_opt.apply blks in
-  let blk2 = List.find opts ~f:(fun b -> Tid.(Term.tid b = Term.tid blk2)) in
-  match Option.map blk2 ~f:(fun b -> Term.enum jmp_t b |> Seq.to_list) with
+  let blk_dir = List.find opts ~f:(fun b -> Tid.(Term.tid b = Term.tid blk_dir)) in
+  match Option.map blk_dir ~f:(fun b -> Term.enum jmp_t b |> Seq.to_list) with
   | None -> assert_failure "didn't find block!"
   | Some [j] ->
     begin
