c8a26150212eb34eb61257f2ea66bcdaa83b8154
Cody Roux <croux@draper.com>
2021-09-30 14:26:29 -0400

    Add custom loader test to the suite.

--- bin/system-tests/tests/test-arm-exes.bash
+++ bin/system-tests/tests/test-arm-exes.bash
@@ -112,6 +112,24 @@ test_arm_subst_stack () {
     run_arm_exe "${TEST_PATCH_EXE}" 3
 }
 
+test_arm_vibes_loader () {
+    local TEST_DIR="${EXES_DIR}/arm-vibes-loader"
+    local MAIN_EXE="${TEST_DIR}/main.reference"
+    local PATCH_EXE="${TEST_DIR}/main.patched.reference"
+    local TEST_PATCH_EXE="${TEST_DIR}/test.patched.by.vibes"
+
+    print_header "Checking ${TEST_DIR}"
+
+    # Check the precompiled executables.
+    run_arm_exe "${MAIN_EXE}" 5
+    run_arm_exe "${PATCH_EXE}" 3
+
+    # Check that vibes patches correctly.
+    run_make "make clean -C ${TEST_DIR}" 0
+    run_make "make patch.reference -C ${TEST_DIR}" 0
+    run_arm_exe "${TEST_PATCH_EXE}" 3
+}
+
 # Run these tests:
 test_arm_simple
 test_arm_simple_cegis
@@ -119,3 +137,4 @@ test_arm_simple_compiled
 test_arm_simple_multi
 test_arm_subst_reg
 test_arm_subst_stack
+test_arm_vibes_loader
