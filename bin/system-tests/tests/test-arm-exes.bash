63438f3182e68e2bdd82950511b4b0a673c22c68
Cody Roux <croux@draper.com>
2021-10-05 17:04:27 -0400

    Add a test for the new call syntax.

--- bin/system-tests/tests/test-arm-exes.bash
+++ bin/system-tests/tests/test-arm-exes.bash
@@ -166,6 +166,24 @@ test_arm_stripped_loader () {
     run_arm_exe "${TEST_PATCH_EXE}" 3
 }
 
+test_arm_patch_call () {
+    local TEST_DIR="${EXES_DIR}/arm-patch-call"
+    local MAIN_EXE="${TEST_DIR}/main.reference"
+    local PATCH_EXE="${TEST_DIR}/main.patched.reference"
+    local TEST_PATCH_EXE="${TEST_DIR}/test.patched.by.vibes"
+
+    print_header "Checking ${TEST_DIR}"
+
+    # Check the precompiled executables.
+    run_arm_exe "${MAIN_EXE}" 5
+    run_arm_exe "${PATCH_EXE}" 3
+
+    # Check that vibes patches correctly.
+    run_make "make clean -C ${TEST_DIR}" 0
+    run_make "make patch.reference -C ${TEST_DIR}" 0
+    run_arm_exe "${TEST_PATCH_EXE}" 3
+}
+
 # Run these tests:
 test_arm_simple
 test_arm_simple_inline
