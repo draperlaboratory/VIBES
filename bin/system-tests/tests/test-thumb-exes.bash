37b0d2097e202741fc9f2f51355dfea9e36d9b2f
Chris Casinghino <chris.casinghino@gmail.com>
2021-10-10 18:06:14 -0400

    Add more complex custom loader test case.

--- bin/system-tests/tests/test-thumb-exes.bash
+++ bin/system-tests/tests/test-thumb-exes.bash
@@ -76,10 +76,29 @@ test_thumb_simple_multi () {
     run_arm_exe "${TEST_PATCH_EXE}" 1
 }
 
-# Run all tests
+test_thumb_stripped_loader_multi () {
+    local TEST_DIR="${EXES_DIR}/thumb-stripped-loader-multi"
+    local MAIN_EXE="${TEST_DIR}/main.reference"
+    local PATCH_EXE="${TEST_DIR}/main.patched.reference"
+    local TEST_PATCH_EXE="${TEST_DIR}/test.patched.by.vibes"
+
+    print_header "Checking ${TEST_DIR}"
+
+    # Check the precompiled executables.
+    run_arm_exe "${MAIN_EXE}" 10
+    run_arm_exe "${PATCH_EXE}" 114
+
+    # Check that vibes patches correctly.
+    run_make "make clean -C ${TEST_DIR}" 0
+    run_make "make patch.reference -C ${TEST_DIR}" 0
+    run_arm_exe "${TEST_PATCH_EXE}" 114
+}
+
+# Run all tests.
 run_all () {
     test_thumb_simple
     test_thumb_simple_cegis
     test_thumb_simple_compiled
     test_thumb_simple_multi
+    test_thumb_stripped_loader_multi
 }
