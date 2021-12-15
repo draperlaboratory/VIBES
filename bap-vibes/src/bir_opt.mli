e598ffa0371a87993da8233ab73653485142c1b3
Benjamin Mourad <bmourad@draper.com>
2021-12-15 09:02:56 -0500

    Adds docstring for `Bir_opt.apply_ordered`

--- bap-vibes/src/bir_opt.mli
+++ bap-vibes/src/bir_opt.mli
@@ -9,4 +9,7 @@ open Bap.Std
 
 val apply : blk term list -> blk term list
 
+(** [apply_ordered blks] will attempt to merge adjacent blocks which have an
+    edge in between them. For this transformation, [blks] must be ordered
+    according to a reverse postorder DFS traversal. *)
 val apply_ordered : blk term list -> blk term list
