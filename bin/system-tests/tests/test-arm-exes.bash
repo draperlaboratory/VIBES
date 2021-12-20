f1eddacf7f54a00a64ada74fb860c8355bb1e336
bmourad01 <bmourad@draper.com>
2021-12-20 13:12:24 -0500

    Adds simple test with metadata
    Also fixes the arm-branch test

--- bin/system-tests/tests/test-arm-exes.bash
+++ bin/system-tests/tests/test-arm-exes.bash
@@ -220,6 +220,46 @@ test_arm_linear_ssa () {
     run_arm_exe "${TEST_PATCH_EXE}" 3
 }
 
+test_arm_branch () {
+    local TEST_DIR="${EXES_DIR}/arm-branch"
+    local MAIN_EXE="${TEST_DIR}/main.reference"
+    local PATCH_EXE="${TEST_DIR}/main.patched.reference"
+    local TEST_PATCH_EXE="${TEST_DIR}/main.patched"
+
+    print_header "Checking ${TEST_DIR}"
+
+    # Check the precompiled executables.
+    run_make "make main -C ${TEST_DIR}" 0
+    run_make "make main.patched.reference -C ${TEST_DIR}" 0
+    run_arm_exe "${MAIN_EXE}" 1
+    run_arm_exe "${PATCH_EXE}" 2
+
+    # Check that vibes patches correctly.
+    run_make "make clean -C ${TEST_DIR}" 0
+    run_make "make main.patched -C ${TEST_DIR}" 0
+    run_arm_exe "${TEST_PATCH_EXE}" 2
+}
+
+test_arm_branch_bsi () {
+    local TEST_DIR="${EXES_DIR}/arm-branch/bsi"
+    local MAIN_EXE="${TEST_DIR}/main.reference"
+    local PATCH_EXE="${TEST_DIR}/main.patched.reference"
+    local TEST_PATCH_EXE="${TEST_DIR}/main.patched"
+
+    print_header "Checking ${TEST_DIR}"
+
+    # Check the precompiled executables.
+    run_make "make main -C ${TEST_DIR}" 0
+    run_make "make main.patched.reference -C ${TEST_DIR}" 0
+    run_arm_exe "${MAIN_EXE}" 1
+    run_arm_exe "${PATCH_EXE}" 2
+
+    # Check that vibes patches correctly.
+    run_make "make clean -C ${TEST_DIR}" 0
+    run_make "make main.patched -C ${TEST_DIR}" 0
+    run_arm_exe "${TEST_PATCH_EXE}" 2
+}
+
 # Run all tests
 run_all () {
     test_arm_simple
@@ -231,4 +271,6 @@ run_all () {
     test_arm_subst_stack
     test_arm_vibes_loader
     test_arm_stripped_loader
+    test_arm_branch
+    test_arm_branch_bsi
 }
