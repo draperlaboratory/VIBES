0043d7ede0863990fab59069fa53b3b221c00527
bmourad01 <bmourad@draper.com>
2022-02-23 12:23:04 -0500

    Don't print out the BIR a second time

--- bap-vibes/src/compiler.ml
+++ bap-vibes/src/compiler.ml
@@ -57,15 +57,9 @@ let create_vibes_ir
           Arm.ARM_Gen.select ir
             ~patch:(Some patch) ~argument_tids ~is_thumb
         | Some isel_model_filepath ->
-          begin
-            Events.(send @@ Info "Running Minizinc instruction selector.");
-            let* ir = KB.List.map ~f:Flatten.flatten_blk ir in
-            List.iter ir ~f:(fun blk ->
-            Events.(send @@ Info (sprintf "The patch has the following BIL: %a" Blk.pps blk)));
-            let* ir = Isel.run ~isel_model_filepath ir Arm.Isel.patterns in
-            KB.return ir
-          end
-      in
+          Events.(send @@ Info "Running Minizinc instruction selector.");
+          let* ir = Isel.run ~isel_model_filepath ir Arm.Isel.patterns in
+          KB.return ir in
       Arm.preassign tgt ir ~is_thumb
     else Kb_error.(fail @@ Other (
         sprintf "Unsupported lang %s" (Theory.Language.to_string lang))) in
