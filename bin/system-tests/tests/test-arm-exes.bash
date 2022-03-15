ffa4eceb8879d5a183562cb99fd865f258c9b073
bmourad01 <bmourad@draper.com>
2022-03-15 12:25:18 -0400

    Adds the `password` example

--- bin/system-tests/tests/test-arm-exes.bash
+++ bin/system-tests/tests/test-arm-exes.bash
@@ -371,6 +371,27 @@ test_arm_null_check () {
     run_arm_exe "${TEST_PATCH_EXE}" 5
 }
 
+test_arm_password () {
+    local TEST_DIR="${EXES_DIR}/arm-password"
+    local MAIN_EXE="${TEST_DIR}/main"
+    local PATCH_EXE="${TEST_DIR}/main.patched.reference"
+    local TEST_PATCH_EXE="${TEST_DIR}/main.patched"
+
+    print_header "Checking ${TEST_DIR}"
+
+    run_make "make clean -C ${TEST_DIR}" 0
+
+    # Check the precompiled executables.
+    run_make "make main -C ${TEST_DIR}" 0
+    run_make "make main.patched.reference -C ${TEST_DIR}" 0
+    run_arm_exe "${MAIN_EXE}" 255
+    run_arm_exe "${PATCH_EXE}" 0
+
+    # Check that vibes patches correctly.
+    run_make "make main.patched -C ${TEST_DIR}" 0
+    run_arm_exe "${TEST_PATCH_EXE}" 0
+}
+
 # Run all tests
 run_all () {
     test_arm_simple
@@ -390,4 +411,5 @@ run_all () {
     test_arm_branch_bsi
     test_arm_bounds_check
     test_arm_null_check
+    test_arm_password
 }
