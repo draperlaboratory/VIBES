6d4b25c1693ecd26cca84e21b0f5bfe38852a560
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2022-04-05 12:30:36 -0400

    Separates the BIR helpers to a new module (#198)
    Separates the BIR helpers to a new module
    
    Co-authored-by: bmourad01 <bmourad@draper.com>

--- bap-vibes/src/bir_passes.ml
+++ bap-vibes/src/bir_passes.ml
@@ -23,6 +23,7 @@ module Subst = Substituter
 module Naming = Subst.Naming
 module Hvar = Higher_var
 module Err = Kb_error
+module Helper = Bir_helpers
 
 type t = {
   ir : blk term list;
@@ -30,77 +31,6 @@ type t = {
   exclude_regs : String.Set.t;
   argument_tids : Tid.Set.t;
 }
-
-(* Helper functions *)
-module Helper = struct
-
-  (* Create a dummy subroutine from the blocks. *)
-  let create_sub (blks : blk term list) : sub term KB.t =
-    let+ tid = Theory.Label.fresh in
-    Sub.create ~name:"dummy-wrapper" ~blks ~tid ()
-
-  (* Find the exit nodes of the patch code. They are classified as follows:
-
-     - no jmps:
-       this is implicitly an exit block since it has no successors
-       in the CFG.
-
-     - goto <indirect>:
-       this block is explicitly jumping out of the program
-       to somewhere else in the binary.
-
-     - call <x> with noreturn:
-       this block must implicitly return to an exit block.
-
-     - call <x> with return <indirect>:
-       this block will not be returning to the patch code.
-  *)
-  let exit_blks (blks : blk term list) : blk term list KB.t =
-    match blks with
-    | [] -> Kb_error.fail @@ Other
-        "Bir_passes.Helper.exit_blks: got an empty list of blks"
-    | [_] -> KB.return blks
-    | _ ->
-      let+ sub = create_sub blks in
-      let cfg = Sub.to_cfg sub in
-      Graphs.Ir.nodes cfg |> Seq.to_list |>
-      List.filter_map ~f:(fun node ->
-          let blk = Graphs.Ir.Node.label node in
-          if Graphs.Ir.Node.degree node cfg ~dir:`Out = 0
-          then Some blk
-          else
-            let jmps = Term.enum jmp_t blk in
-            if Seq.is_empty jmps then Some blk
-            else if Seq.exists jmps ~f:(fun jmp ->
-                match Jmp.kind jmp with
-                | Call call -> begin
-                    match Call.return call with
-                    | None | Some (Indirect _) -> true
-                    | _ -> false
-                  end
-                | _ -> false)
-            then Some blk
-            else None)
-
-  (* Returns true if the block contains a call to a subroutine. *)
-  let has_call (blk : blk term) : bool =
-    Term.enum jmp_t blk |> Seq.exists ~f:(fun jmp ->
-        match Jmp.kind jmp with
-        | Call _ -> true
-        | _ -> false)
-
-  (* Returns true if there are calls in the IR. *)
-  let call_blks (blks : blk term list) : tid list =
-    List.filter_map blks ~f:(fun blk ->
-        if has_call blk then Some (Term.tid blk) else None)
-
-  (* Returns true if the jmp is unconditional. *)
-  let is_unconditional (jmp : jmp term) : bool =
-    match Jmp.cond jmp with
-    | Int w -> Word.(w = b1)
-    | _ -> false
-
-end
 
 (* BIR optimizations. All of these passes assume that the blocks are ordered
    according to `Shape.reorder_blks`, and that the program is NOT YET in SSA
@@ -235,9 +165,8 @@ module Opt = struct
             Tid.Table.set blk_table ~key:(Term.tid blk) ~data:blk);
         (* Get the implicit exit block. *)
         let exit_tid = List.find_map blks ~f:(fun blk ->
-            if Seq.is_empty @@ Term.enum jmp_t blk
-            then Some (Term.tid blk)
-            else None) in
+            if Helper.is_implicit_exit blk
+            then Some (Term.tid blk) else None) in
         (* We can merge with a successor block if we are its only predecessor.
            Note that the pseudo-start node will be a predecessor of
            "unreachable" blocks. Furthermore, the implicit exit block cannot
@@ -646,8 +575,7 @@ module Shape = struct
       Seq.to_list |> List.map ~f:Graphs.Ir.Node.label in
     (* The exit block with no jmps should be ordered at the very end of
        the program. *)
-    match List.find blks ~f:(fun blk ->
-        Seq.is_empty @@ Term.enum jmp_t blk) with
+    match List.find blks ~f:Helper.is_implicit_exit with
     | None -> blks
     | Some blk ->
       let tid = Term.tid blk in
