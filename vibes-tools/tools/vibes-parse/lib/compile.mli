6b641b1c88bc05cd559158008721417168037b5c
bmourad01 <bmourad@draper.com>
2022-08-10 10:28:42 -0400

    Fix comment

--- vibes-tools/tools/vibes-parse/lib/compile.mli
+++ vibes-tools/tools/vibes-parse/lib/compile.mli
@@ -1,8 +1,10 @@
 open Bap_core_theory
 open Vibes_higher_vars
 
-(** [to_core ast target hvars] provides the semantics of the parsed C
-    program to all currently available theories. *)
+(** [to_core ast target hvars] compiles the parsed C program [ast] into
+    a Core Theory program and promises the semantics, which should then
+    be available to any of the currently instantiated theories (such as
+    BIL, BIR, etc). *)
 val to_core :
   Types.ast ->
   Theory.target ->
