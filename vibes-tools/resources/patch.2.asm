c821a417042d6ac1812ed1e332fe93cac6da2fdf
bmourad01 <bmourad@draper.com>
2022-08-31 18:20:17 -0400

    Try with the `constant` field for `foo` in the second example

--- vibes-tools/resources/patch.2.asm
+++ vibes-tools/resources/patch.2.asm
@@ -1,4 +1,7 @@
 ((directives (.syntax_unified))
  (blocks
-  (((label blk0000001b) (insns ("mov R0, #3" "mov R1, #6" "bl blk0000001c")))
-   ((label blk0000001a) (insns ("mov R1, R0"))))))
+  (((label blk0000000b)
+    (insns
+     ("mov R0, #3" "mov R1, #6"
+      "bl (patch_start_label + 4660 - patch_location)")))
+   ((label blk0000000a) (insns ("mov R1, R0"))))))
