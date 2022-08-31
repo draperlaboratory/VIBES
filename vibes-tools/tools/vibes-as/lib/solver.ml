04004348c0fe2e2840733afde43e767e553cf26d
bmourad01 <bmourad@draper.com>
2022-08-31 18:06:31 -0400

    Neaten

--- vibes-tools/tools/vibes-as/lib/solver.ml
+++ vibes-tools/tools/vibes-as/lib/solver.ml
@@ -9,19 +9,23 @@ module Solution = Vibes_minizinc.Types.Solution
 module Minizinc = Vibes_minizinc.Utils
 
 let (let*) x f = Result.bind x ~f
+let (let+) x f = Result.map x ~f
 
 let opt
     (ir : Ir.t)
     (target : T.target) : (Ir.t, KB.conflict) result =
-  let* is_nop, unconditional_branch_target =
+  let+ is_nop, unconditional_branch_target =
     if T.Target.belongs Arm_target.parent target then
-      Ok Arm_utils.(is_nop, unconditional_branch_target)
+      let open Arm_utils in
+      Ok (is_nop, unconditional_branch_target)
     else
       let msg = Format.asprintf
           "Unsupported target %a"
           T.Target.pp target in
       Error (Errors.Unsupported_target msg) in
-  Ok (Opt.peephole ir ~is_nop ~unconditional_branch_target)
+  Opt.peephole ir
+    ~is_nop
+    ~unconditional_branch_target
 
 let solve
     (ir : Ir.t)
@@ -32,5 +36,4 @@ let solve
   let* solution_filepath = Minizinc.run_minizinc params ~model_filepath in
   let* solution = Solution.deserialize solution_filepath info in
   let ir = Solution.apply ir solution in
-  let* ir = opt ir target in
-  Ok ir
+  opt ir target
