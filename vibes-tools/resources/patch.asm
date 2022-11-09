7064843c09e8ee4a7d724cfab7c66a030c487e89
bmourad01 <bmourad@draper.com>
2022-11-09 14:39:39 -0500

    Update example

--- vibes-tools/resources/patch.asm
+++ vibes-tools/resources/patch.asm
@@ -2,4 +2,4 @@
  (directives (".syntax unified"))
  (blocks
   (((label blk00000009)
-    (insns ("movw R10, #48879" "mov R0, #3" "str R0, [R10]"))))))
+    (insns ("movw R8, #48879" "mov R7, #3" "str R7, [R8]"))))))
