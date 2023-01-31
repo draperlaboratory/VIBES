b09c0637a052bb4fa444a9dcda9bfbf0eb5d6bf1
bmourad01 <bmourad@draper.com>
2023-01-31 16:49:23 -0500

    `arm-branch`

---
+++ vibes-tools/resources/exes/arm-branch/vibes/verify.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+run () {
+    bap wp main main.patched \
+        --no-cache \
+        --no-objdump \
+        --ext-solver-path=boolector \
+        --ogre-orig=./vibes/loader.ogre \
+        --ogre-mod=./main.patched.ogre \
+        --func=foo \
+        --show=diagnostics \
+        --postcond="(assert
+(implies
+  (and (= init_mem_orig init_mem_mod)
+       (= init_R0_orig init_R0_mod))
+  (ite (= init_R0_mod #x00000000)
+       (= R0_mod #x00000001)
+       (= R0_mod #x00000002))))"
+}
+
+run
