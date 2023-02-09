7fa420989601618a22ea807ae009bf506b82a65d
bmourad01 <bmourad@draper.com>
2023-02-09 16:27:03 -0500

    Forgot to add the PPC printer

--- tools/vibes-as/lib/utils.ml
+++ tools/vibes-as/lib/utils.ml
@@ -15,13 +15,16 @@ open Bap_core_theory
 
 module T = Theory
 module Utils = Vibes_utils
+module CT = Utils.Core_theory
 
 let asm_printer
     (target : T.target)
     (language : T.language) : (Types.Assembly.printer, KB.conflict) result =
-  if T.Target.belongs Arm_target.parent target then
+  if CT.is_arm32 target then
     let is_thumb = Utils.Core_theory.is_thumb language in
     Ok (Arm_printer.ir ~is_thumb)
+  else if CT.is_ppc32 target then
+    Ok Ppc_printer.ir
   else
     let msg = Format.asprintf
         "Unsupported target %a"
